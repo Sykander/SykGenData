ca90d3b19822d8a251fe7ccfeda604984bffffd3
Sykander <sgul@spartaglobal.com>
2018-07-02 21:21:12 +0100

    adjusted the cc generator to only load the json file once instead of everytime a cc number is generated

--- lib/SykGenData/generators/gen_cc_data.rb
+++ lib/SykGenData/generators/gen_cc_data.rb
@@ -2,10 +2,18 @@ require 'json'
 
 class GenCCData
 
+  def initialize
+    @cc_visa_hash = nil
+    @cc_amex_hash = nil
+    @cc_mastercard_hash = nil
+  end
+
   def get_visa_cc_number
-    cc_visa_numbers = File.read(Gem.loaded_specs["SykGenData"].gem_dir + '/lib/SykGenData/resources/cc_visa_numbers.json')
-    cc_visa_hash = JSON.parse(cc_visa_numbers)
-    cc_number = cc_visa_hash.sample['CreditCard']["CardNumber"].to_s
+    if @cc_visa_hash.nil?
+      cc_visa_numbers = File.read(Gem.loaded_specs["SykGenData"].gem_dir + '/lib/SykGenData/resources/cc_visa_numbers.json')
+      @cc_visa_hash = JSON.parse(cc_visa_numbers)
+    end
+    cc_number = @cc_visa_hash.sample['CreditCard']["CardNumber"].to_s
   end
 
   def get_visa_sec_code
@@ -17,9 +25,11 @@ class GenCCData
   end
 
   def get_amex_cc_number
-    cc_amex_numbers = File.read(Gem.loaded_specs["SykGenData"].gem_dir + '/lib/SykGenData/resources/cc_amex_numbers.json')
-    cc_amex_hash = JSON.parse(cc_amex_numbers)
-    cc_number = cc_amex_hash.sample['CreditCard']['CardNumber'].to_s
+    if @cc_amex_hash.nil?
+      cc_amex_numbers = File.read(Gem.loaded_specs["SykGenData"].gem_dir + '/lib/SykGenData/resources/cc_amex_numbers.json')
+      @cc_amex_hash = JSON.parse(cc_amex_numbers)
+    end
+    cc_number = @cc_amex_hash.sample['CreditCard']['CardNumber'].to_s
   end
 
   def get_amex_sec_code
@@ -31,9 +41,11 @@ class GenCCData
   end
 
   def get_mastercard_cc_number
-    cc_mastercard_numbers = File.read(Gem.loaded_specs["SykGenData"].gem_dir + '/lib/SykGenData/resources/cc_mastercard_numbers.json')
-    cc_mastercard_hash = JSON.parse(cc_mastercard_numbers)
-    cc_number = cc_mastercard_hash.sample['CreditCard']["CardNumber"].to_s
+    if @cc_mastercard_hash.nil?
+      cc_mastercard_numbers = File.read(Gem.loaded_specs["SykGenData"].gem_dir + '/lib/SykGenData/resources/cc_mastercard_numbers.json')
+      @cc_mastercard_hash = JSON.parse(cc_mastercard_numbers)
+    end
+    cc_number = @cc_mastercard_hash.sample['CreditCard']["CardNumber"].to_s
   end
 
   def get_mastercard_sec_code
