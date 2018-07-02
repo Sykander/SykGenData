12a850d4df8b87b972a2f2c48b4f1545fdf1e005
Sykander <sgul@spartaglobal.com>
2018-07-02 21:11:32 +0100

    added tests for the amex card functions

--- spec/gen_cc_data_spec.rb
+++ spec/gen_cc_data_spec.rb
@@ -17,4 +17,14 @@ RSpec.describe "Generate CC data for testing online payment forms" do
     expect(@gen.cc.get_visa_sec_code.length).to eq 3
   end
 
+  it "should return an amex cc number" do
+    expect(@gen.cc.get_amex_cc_number).to be_a String
+    expect(@gen.cc.get_amex_cc_number.length).to eq 15
+  end
+
+  it "should return an amex security code" do
+    expect(@gen.cc.get_amex_sec_code).to be_a String
+    expect(@gen.cc.get_amex_sec_code.length).to eq 4
+  end
+
 end
